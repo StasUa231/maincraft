564e1fa07cc74ef776bd07ce68baae1b6c06fc6f
StasUa231 <141513116+StasUa231@users.noreply.github.com>
2023-08-05 13:36:03 -0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Головна сторінка сайту', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    return render(request, 'main/create.html')
